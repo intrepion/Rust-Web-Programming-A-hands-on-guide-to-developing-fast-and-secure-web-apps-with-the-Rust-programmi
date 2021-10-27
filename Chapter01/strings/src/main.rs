2fa4b36f8c7b32305b42f444235fb47543868388
Oliver Forral <intrepion@gmail.com>
2021-10-27 07:34:28 -0600

    passing a string with borrowing

--- Chapter01/strings/src/main.rs
+++ Chapter01/strings/src/main.rs
@@ -1,8 +1,8 @@
-fn print(input_string: String) {
+fn print(input_string: &str) {
     println!("{}", input_string);
 }
 
 fn main() {
-    let test_string = "Hello, World!";
-    print(test_string.to_string());
+    let test_string = &"Hello, World!";
+    print(test_string);
 }
