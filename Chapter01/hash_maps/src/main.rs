16d0ec2c9e2ba1ad79c384650e482e284d50f4a9
Oliver Forral <intrepion@gmail.com>
2021-10-28 01:47:26 -0600

    inserted an entry into a hashmap and then finding it again

--- Chapter01/hash_maps/src/main.rs
+++ Chapter01/hash_maps/src/main.rs
@@ -1,3 +1,10 @@
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+    let mut general_map: HashMap<&str, i8> = HashMap::new();
+    general_map.insert("test", 25);
+    match general_map.get("test") {
+        None => println!("it failed"),
+        Some(result) => println!("here is the result: {}", result),
+    }
 }
