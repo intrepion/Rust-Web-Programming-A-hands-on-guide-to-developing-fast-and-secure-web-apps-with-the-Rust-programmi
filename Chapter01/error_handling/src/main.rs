f41177b38e81203df4a23bf9814e8712834fd4f1
Oliver Forral <intrepion@gmail.com>
2021-10-28 03:12:14 -0600

    demonstrating how to throw an error

--- Chapter01/error_handling/src/main.rs
+++ Chapter01/error_handling/src/main.rs
@@ -1,3 +1,12 @@
+fn error_check(check: bool) -> Result<i8, &'static str> {
+    if check == true {
+        Err("this is an error")
+    } else {
+        Ok(1)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let result: i8 = error_check(false).unwrap();
+    println!("{}", result);
 }
