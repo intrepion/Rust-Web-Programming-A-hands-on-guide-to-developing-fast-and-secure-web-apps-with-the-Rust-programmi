a9154aeb6e5171280230e1a635f4c900b1d721ac
Oliver Forral <intrepion@gmail.com>
2021-10-27 19:24:59 -0600

    Storing data in vectors and arrays

--- Chapter01/vectors_and_arrays/src/main.rs
+++ Chapter01/vectors_and_arrays/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let int_array: [i32; 3] = [1, 2, 3];
+
+    for i in int_array.iter() {
+        println!("{}", i);
+    }
+
+    let mut str_vector: Vec<&str> = vec!["one", "two", "there"];
+
+    for i in str_vector.iter() {
+        println!("{}", i);
+    }
+
+    let second_int_array: [i32; 3] = [1, 2, 3];
+    let two = second_int_array[1];
+
+    str_vector.push("four");
 }
