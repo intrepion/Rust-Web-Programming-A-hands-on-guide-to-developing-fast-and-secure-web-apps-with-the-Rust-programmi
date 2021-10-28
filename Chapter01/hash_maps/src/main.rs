249a7f9029cf0e8fbbd6dca344ca1578ab47cc2b
Oliver Forral <intrepion@gmail.com>
2021-10-28 02:08:26 -0600

    nesting match statements

--- Chapter01/hash_maps/src/main.rs
+++ Chapter01/hash_maps/src/main.rs
@@ -3,8 +3,13 @@ use std::collections::HashMap;
 fn main() {
     let mut general_map: HashMap<&str, i8> = HashMap::new();
     general_map.insert("test", 25);
-    match general_map.get("test") {
-        None => println!("it failed"),
+    match general_map.get("testing") {
+        None => {
+            match general_map.get("test") {
+                None => println!("Both testing and test failed"),
+                Some(result) => println!("testing failed but test is: {}", result),
+            }
+        },
         Some(result) => println!("here is the result: {}", result),
     }
 }
