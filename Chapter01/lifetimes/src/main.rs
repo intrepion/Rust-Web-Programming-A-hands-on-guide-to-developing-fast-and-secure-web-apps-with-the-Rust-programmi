03c3d2529eae9715e43296bb8e5bd3debaa4644a
Oliver Forral <intrepion@gmail.com>
2021-11-01 06:02:16 -0600

    function where both inputs have different lifetimes

--- Chapter01/lifetimes/src/main.rs
+++ Chapter01/lifetimes/src/main.rs
@@ -1,16 +1,17 @@
-fn get_highest<'a>(first_number: &'a i8, second_number: &'a i8) -> &'a i8 {
-    if first_number > second_number {
-        first_number
+fn filter<'a, 'b>(first_number: &'a i8, second_number: &'b i8) -> &'a i8 {
+    if first_number < second_number {
+        &0
     } else {
-        second_number
+        first_number
     }
 }
 
 fn main() {
     let one: i8 = 1;
+    let outcome: &i8;
     {
         let two: i8 = 2;
-        let outcome: &i8 = get_highest(&one, &two);
+        let outcome = filter(&one, &two);
         println!("{}", outcome);
     }
 }
