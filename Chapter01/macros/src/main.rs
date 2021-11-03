f4c85a5fc29335f0ce10bcc7bcb7b517aaa9ea7f
Oliver Forral <intrepion@gmail.com>
2021-11-02 22:26:42 -0600

    define a macro that capitalizes a string

--- Chapter01/macros/src/main.rs
+++ Chapter01/macros/src/main.rs
@@ -1,10 +1,13 @@
-struct Coordinate <T> {
-    x: T,
-    y: T,
+macro_rules! capitalize {
+    ($a: expr) => {
+        let mut v: Vec<char> = $a.chars().collect();
+        v[0] = v[0].to_uppercase().nth(0).unwrap();
+        $a = v.into_iter().collect();
+    }
 }
 
 fn main() {
-    let one = Coordinate {x: 50, y: 50};
-    let two = Coordinate {x: 500, y: 500};
-    let three = Coordinate {x: 5.6, y: 5.6};
+    let mut x = String::from("test");
+    capitalize!(x);
+    println!("{}", x);
 }
