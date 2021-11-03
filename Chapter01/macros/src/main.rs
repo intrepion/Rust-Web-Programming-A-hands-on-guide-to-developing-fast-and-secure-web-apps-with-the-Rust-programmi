67e9643f97773e5eb17f6f461257e00f99f0ca28
Oliver Forral <intrepion@gmail.com>
2021-11-02 22:03:08 -0600

    demonstrating generics through coordinates

--- Chapter01/macros/src/main.rs
+++ Chapter01/macros/src/main.rs
@@ -1,3 +1,10 @@
+struct Coordinate <T> {
+    x: T,
+    y: T,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let one = Coordinate {x: 50, y: 50};
+    let two = Coordinate {x: 500, y: 500};
+    let three = Coordinate {x: 5.6, y: 5.6};
 }
