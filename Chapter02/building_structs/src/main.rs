29686dd14fcab256e3e336386315dc044b018fce
Oliver Forral <intrepion@gmail.com>
2021-11-03 18:53:26 -0600

    Managing structs with factories

--- Chapter02/building_structs/src/main.rs
+++ Chapter02/building_structs/src/main.rs
@@ -1,14 +1,21 @@
 mod to_do;
 
-use to_do::structs::done::Done;
-use to_do::structs::pending::Pending;
+use to_do::ItemTypes;
+use to_do::to_do_factory;
 
 fn main() {
-    let done: Done = Done::new("shopping");
-    println!("{}", done.super_struct.title);
-    println!("{}", done.super_struct.status);
+    let to_do_item: Result<ItemTypes, &'static str> =
+        to_do_factory("pending", "make");
 
-    let pending: Pending = Pending::new("laundry");
-    println!("{}", pending.super_struct.title);
-    println!("{}", pending.super_struct.status);
+    match to_do_item.unwrap() {
+        ItemTypes::Pending(item) => println!(
+            "it's a pending item with the title: {}",
+            item.super_struct.title
+        ),
+
+        ItemTypes::Done(item) => println!(
+            "it's a done item with the title: {}",
+            item.super_struct.title
+        ),
+    }
 }
