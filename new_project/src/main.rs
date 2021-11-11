5a932fd0db373d590d80f9dd09b41114407cb006
Oliver Forral <intrepion@gmail.com>
2021-11-11 01:06:06 -0700

    wrote first test

--- new_project/src/main.rs
+++ new_project/src/main.rs
@@ -1,3 +1,25 @@
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn handle_check_new_project_with_no_environment_arguments() {
+        let expected = "Missing environment arguments";
+        let actual = check_new_project(vec!["new_project".to_string()]).unwrap_err();
+        assert_eq!(expected, actual);
+    }
+}
+
+fn check_new_project(_arguments: Vec<String>) -> Result<(), &'static str> {
+    return Err("Missing environment arguments");
+}
+
 fn main() {
-    println!("Hello, world!");
+    use std::env;
+
+    let result = check_new_project(env::args().collect());
+    match result {
+        Ok(o) => println!("okay: {:?}", o),
+        Err(e) => println!("error: {:?}", e),
+    }
 }
