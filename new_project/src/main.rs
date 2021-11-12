79053d31475b7ec667fc3931cd1a7c8e2d4ad376
Oliver Forral <intrepion@gmail.com>
2021-11-11 23:35:02 -0700

    adding more newlines to help things be clearer

--- new_project/src/main.rs
+++ new_project/src/main.rs
@@ -4,10 +4,13 @@ mod test {
 
     #[test]
     fn handle_check_new_project_with_missing_page_number() {
+
         let expected = "Missing page number.";
+
         let arguments = vec![
             "new_project".to_string(),
         ];
+
         let actual = check_new_project(arguments).unwrap_err();
 
         assert_eq!(expected, actual);
@@ -15,12 +18,15 @@ mod test {
 
     #[test]
     fn handle_check_new_project_with_an_english_page_number() {
+
         let expected = "Invalid page number.";
+
         let arguments = vec![
             "new_project".to_string(),
             "one".to_string(),
             "hello_world".to_string(),
         ];
+
         let actual = check_new_project(arguments).unwrap_err();
 
         assert_eq!(expected, actual);
@@ -28,11 +34,14 @@ mod test {
 
     #[test]
     fn handle_check_new_project_with_missing_project_name() {
+
         let expected = "Missing project name.";
+
         let arguments = vec![
             "new_project".to_string(),
             "1".to_string(),
         ];
+
         let actual = check_new_project(arguments).unwrap_err();
 
         assert_eq!(expected, actual);
@@ -40,12 +49,15 @@ mod test {
 
     #[test]
     fn handle_check_new_project_with_empty_project_name() {
+
         let expected = "Empty project name.";
+
         let arguments = vec![
             "new_project".to_string(),
             "1".to_string(),
             " ".to_string(),
         ];
+
         let actual = check_new_project(arguments).unwrap_err();
 
         assert_eq!(expected, actual);
@@ -53,15 +65,18 @@ mod test {
 
     #[test]
     fn handle_check_new_project_happy_path() {
+
         let expected = Project {
             name: "hello".to_string(),
             page_number: 1,
         };
+
         let arguments = vec![
             "new_project".to_string(),
             "1".to_string(),
             "hello".to_string(),
         ];
+
         let actual = check_new_project(arguments).unwrap();
 
         assert_eq!(expected, actual);
@@ -75,6 +90,7 @@ struct Project {
 }
 
 fn check_new_project(arguments: Vec<String>) -> Result<Project, &'static str> {
+
     if arguments.len() == 1
     {
         return Err("Missing page number.");
@@ -91,6 +107,7 @@ fn check_new_project(arguments: Vec<String>) -> Result<Project, &'static str> {
     }
 
     return match arguments[1].parse::<u32>() {
+
         Ok(n) => Ok(Project {
             name: arguments[2].to_string(),
             page_number: n,
@@ -100,10 +117,13 @@ fn check_new_project(arguments: Vec<String>) -> Result<Project, &'static str> {
 }
 
 fn main() {
+
     use std::env;
 
     let result = check_new_project(env::args().collect());
+
     match result {
+
         Ok(o) => println!("okay: {:?}", o),
         Err(e) => println!("error: {:?}", e),
     }
