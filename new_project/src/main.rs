f8ab3afd1e3f4486e15245cefe5f219d87665042
Oliver Forral <intrepion@gmail.com>
2021-11-11 19:30:08 -0700

    Added test to check for a project name

--- new_project/src/main.rs
+++ new_project/src/main.rs
@@ -3,7 +3,7 @@ mod test {
     use super::*;
 
     #[test]
-    fn handle_check_new_project_with_no_environment_arguments() {
+    fn handle_check_new_project_with_missing_page_number() {
         let expected = "Missing page number.";
         let arguments = vec![
             "new_project".to_string(),
@@ -14,22 +14,41 @@ mod test {
     }
 
     #[test]
-    fn handle_check_new_project_with_an_invalid_page_number() {
+    fn handle_check_new_project_with_an_english_page_number() {
         let expected = "Invalid page number.";
         let arguments = vec![
             "new_project".to_string(),
             "one".to_string(),
+            "hello_world".to_string(),
         ];
         let actual = check_new_project(arguments).unwrap_err();
 
         assert_eq!(expected, actual);
     }
+
+    #[test]
+    fn handle_check_new_project_with_missing_project_name() {
+        let expected = "Missing project name.";
+        let arguments = vec![
+            "new_project".to_string(),
+            "1".to_string(),
+        ];
+        let actual = check_new_project(arguments).unwrap_err();
+
+        assert_eq!(expected, actual);
+    }
+
 }
 
 fn check_new_project(arguments: Vec<String>) -> Result<(), &'static str> {
     if arguments.len() == 1
     {
         return Err("Missing page number.");
+    }
+
+    if arguments.len() == 2
+    {
+        return Err("Missing project name.");
     }
 
     return Err("Invalid page number.");
