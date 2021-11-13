027799be38e672288149d0e819a7ff2076991b35
Oliver Forral <intrepion@gmail.com>
2021-11-13 16:04:19 -0700

    using strings in rust

--- projects/page_005_using_strings_in_rust/src/main.rs
+++ projects/page_005_using_strings_in_rust/src/main.rs
@@ -1,3 +1,25 @@
+#[cfg(test)]
+mod should {
+    use super::*;
+
+    #[test]
+    fn succeed_print_string_when_run() {
+
+        let expected = ();
+        let actual = print_string("Hello, World!".to_string());
+
+        assert_eq!(expected, actual);
+    }
+}
+
+fn print_string(input_string: String) {
+
+    println!("{}", input_string);
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let test_string = String::from("Hello, World!");
+
+    print_string(test_string);
 }
