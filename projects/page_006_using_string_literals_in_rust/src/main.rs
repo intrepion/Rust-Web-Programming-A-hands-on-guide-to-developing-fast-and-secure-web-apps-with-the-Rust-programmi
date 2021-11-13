419e70944b00d1cf2af78b71acc69cba60966f2d
Oliver Forral <intrepion@gmail.com>
2021-11-13 16:19:51 -0700

    using string literals in rust

--- projects/page_006_using_string_literals_in_rust/src/main.rs
+++ projects/page_006_using_string_literals_in_rust/src/main.rs
@@ -1,3 +1,25 @@
+#[cfg(test)]
+mod should {
+    use super::*;
+
+    #[test]
+    fn succeed_print_string_when_run() {
+
+        let expected = ();
+        let actual = print_string("Hello, World!");
+
+        assert_eq!(expected, actual);
+    }
+}
+
+fn print_string(input_string: &str) {
+
+    println!("{}", input_string);
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let test_string = &"Hello, World!";
+
+    print_string(test_string);
 }
