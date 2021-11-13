431c2cc4442f4a4ab82a8056ce4048259a4ff6e1
Oliver Forral <intrepion@gmail.com>
2021-11-12 19:17:57 -0700

    Added last page

--- new_project/src/main.rs
+++ new_project/src/main.rs
@@ -1,9 +1,9 @@
 #[cfg(test)]
-mod test {
+mod should {
     use super::*;
 
     #[test]
-    fn handle_check_new_project_with_missing_page_number() {
+    fn error_when_check_new_project_with_missing_page_number() {
 
         let expected = "Missing page number.";
 
@@ -15,15 +15,31 @@ mod test {
 
         assert_eq!(expected, actual);
     }
+    
+    #[test]
+    fn error_when_check_new_project_missing_last_page() {
+
+        let expected = "Missing last page.";
+
+        let arguments = vec![
+            "new_project".to_string(),
+            "1".to_string(),
+        ];
+
+        let actual = check_new_project(arguments).unwrap_err();
+
+        assert_eq!(expected, actual);
+    }
 
     #[test]
-    fn handle_check_new_project_with_an_english_page_number() {
+    fn error_when_check_new_project_with_an_english_page_number() {
 
         let expected = "Invalid page number.";
 
         let arguments = vec![
             "new_project".to_string(),
             "one".to_string(),
+            "256".to_string(),
             "hello_world".to_string(),
         ];
 
@@ -33,13 +49,14 @@ mod test {
     }
 
     #[test]
-    fn handle_check_new_project_with_missing_project_name() {
+    fn error_when_check_new_project_with_missing_project_name() {
 
         let expected = "Missing project name.";
 
         let arguments = vec![
             "new_project".to_string(),
             "1".to_string(),
+            "256".to_string(),
         ];
 
         let actual = check_new_project(arguments).unwrap_err();
@@ -48,13 +65,14 @@ mod test {
     }
 
     #[test]
-    fn handle_check_new_project_with_empty_project_name() {
+    fn error_when_check_new_project_with_empty_project_name() {
 
         let expected = "Empty project name.";
 
         let arguments = vec![
             "new_project".to_string(),
             "1".to_string(),
+            "256".to_string(),
             " ".to_string(),
         ];
 
@@ -64,9 +82,10 @@ mod test {
     }
 
     #[test]
-    fn handle_check_new_project_happy_path() {
+    fn succeed_when_check_new_project_happy_path() {
 
         let expected = Project {
+            last_page: 256,
             name: "hello".to_string(),
             page_number: 1,
         };
@@ -74,6 +93,7 @@ mod test {
         let arguments = vec![
             "new_project".to_string(),
             "1".to_string(),
+            "256".to_string(),
             "hello".to_string(),
         ];
 
@@ -85,6 +105,7 @@ mod test {
 
 #[derive(Debug, PartialEq)]
 struct Project {
+    last_page: u32,
     name: String,
     page_number: u32,
 }
@@ -98,20 +119,30 @@ fn check_new_project(arguments: Vec<String>) -> Result<Project, &'static str> {
 
     if arguments.len() == 2
     {
+        return Err("Missing last page.");
+    }
+
+    if arguments.len() == 3
+    {
         return Err("Missing project name.");
     }
 
-    if arguments[2].trim().eq("")
+    if arguments[3].trim().eq("")
     {
         return Err("Empty project name.");
     }
 
     return match arguments[1].parse::<u32>() {
 
-        Ok(n) => Ok(Project {
-            name: arguments[2].to_string(),
-            page_number: n,
-        }),
+        Ok(page_number) => match arguments[2].parse::<u32>() {
+            
+            Ok(last_page) => Ok(Project {
+                last_page: last_page,
+                name: arguments[3].to_string(),
+                page_number: page_number,
+            }),
+            Err(_) => Err("Invalid"),
+        },
         Err(_) => Err("Invalid page number."),
     }
 }
@@ -124,7 +155,7 @@ fn main() {
 
     match result {
 
-        Ok(o) => println!("okay: {:?}", o),
-        Err(e) => println!("error: {:?}", e),
+        Ok(project) => println!("okay: {:?}", project),
+        Err(error) => println!("error: {:?}", error),
     }
 }
