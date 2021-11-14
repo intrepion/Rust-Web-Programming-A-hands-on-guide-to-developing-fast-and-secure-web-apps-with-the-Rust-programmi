5ca06bdb2c19543cc18926b1708677b0b8c03026
Oliver Forral <intrepion@gmail.com>
2021-11-13 17:01:54 -0700

    understanding integers and floats

--- projects/page_007_understanding_integers_and_floats/src/main.rs
+++ projects/page_007_understanding_integers_and_floats/src/main.rs
@@ -1,3 +1,27 @@
+#[cfg(test)]
+mod should {
+    use super::*;
+
+    #[test]
+    fn succeed_some_function_when_run() {
+
+        let expected = ();
+        let actual = some_function();
+
+        assert_eq!(expected, actual);
+    }
+}
+
+fn some_function() {
+
+    let _number: u8 = 255;
+
+    let _float: f32 = 20.6;
+
+    let _x = 1u8;
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    some_function();
 }
