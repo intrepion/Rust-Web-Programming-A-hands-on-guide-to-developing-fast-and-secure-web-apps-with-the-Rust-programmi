9c40599fd93452d3956050ce80f9cfe2ed322211
Oliver Forral <intrepion@gmail.com>
2021-11-13 20:24:47 -0700

    handling results and errors

--- projects/page_011_handling_results_and_errors/src/main.rs
+++ projects/page_011_handling_results_and_errors/src/main.rs
@@ -1,3 +1,52 @@
+#[cfg(test)]
+mod should {
+    use super::*;
+
+    #[test]
+    fn succeed_error_check_when_false() {
+
+        let expected = Ok(1);
+        let actual = error_check(false);
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn error_error_check_when_true() {
+
+        let expected = Err("this is an error");
+        let actual = error_check(true);
+
+        assert_eq!(expected, actual);
+    }
+}
+
+fn error_check(check: bool) -> Result<i8, &'static str> {
+
+    if check == true {
+
+        Err("this is an error")
+    } else {
+
+        Ok(1)
+    }
+}
+
+fn describe_result(result: Result<i8, &'static str>) {
+
+    match result {
+
+        Ok(x) => println!("it's a result of: {}", x),
+        Err(x) => println!("{}", x),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let result: Result<i8, &'static str> = error_check(true);
+    describe_result(result);
+
+    if result.is_err() {
+        panic!("throwing some error");
+    }
 }
