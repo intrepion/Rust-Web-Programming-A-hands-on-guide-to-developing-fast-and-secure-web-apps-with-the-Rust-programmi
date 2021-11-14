95c80f0ffc6f92f94eb3bb024087ca6b024a02e4
Oliver Forral <intrepion@gmail.com>
2021-11-13 18:43:55 -0700

    mapping data with hash maps

--- projects/page_009_mapping_data_with_hash_maps/src/main.rs
+++ projects/page_009_mapping_data_with_hash_maps/src/main.rs
@@ -1,3 +1,39 @@
+use std::collections::HashMap;
+
+#[cfg(test)]
+mod should {
+    use super::*;
+
+    #[test]
+    fn succeed_some_function_when_run() {
+
+        let expected = ();
+        let actual = some_function();
+
+        assert_eq!(expected, actual);
+    }
+}
+
+fn some_function() {
+    let mut general_map: HashMap<&str, i8> =
+        HashMap::new();
+
+    general_map.insert("test", 25);
+
+    let _outcome: Option<&i8> = general_map.get("test");
+
+    match general_map.get("testing") {
+        None => {
+            match general_map.get("test") {
+                None => println!("Both testing and test failed"),
+                Some(result) => println!("testing failed but test is: {}", result),
+            }
+        },
+        Some(result) => println!("Here is the result: {}", result),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    some_function();
 }
