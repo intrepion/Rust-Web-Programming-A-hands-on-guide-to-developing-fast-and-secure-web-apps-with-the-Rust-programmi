11528a5de9638ce955ab34019d00a233003c2e89
Oliver Forral <intrepion@gmail.com>
2021-11-13 22:06:23 -0700

    added more scopes

--- projects/page_015_scopes/src/main.rs
+++ projects/page_015_scopes/src/main.rs
@@ -3,10 +3,20 @@ mod should {
     use super::*;
 
     #[test]
-    fn succeed_alter_number_when_run() {
+    fn succeed_alter_text_when_run() {
 
         let expected = "1!".to_string();
         let mut actual = "1".to_string();
+
+        alter_text(&mut actual);
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn succeed_alter_number_when_run() {
+        let expected = 2;
+        let mut actual = 1;
 
         alter_number(&mut actual);
 
@@ -14,25 +24,43 @@ mod should {
     }
 }
 
-fn alter_number(number: &mut String) {
+fn alter_text(text: &mut String) {
 
-    number.push("!".chars().next().unwrap());
+    text.push("!".chars().next().unwrap());
 }
 
-fn print_number(number: &String) {
+fn print_text(text: &String) {
 
+    println!("{}", text);
+}
+
+fn alter_number(number: &mut i8) {
+    *number += 1
+}
+
+fn print_number(number: i8) {
     println!("{}", number);
 }
 
 fn main() {
 
-    let mut one: String = String::from("one");
+    let mut foo: String = String::from("foo");
+    {
+        println!("{}", foo);
+        let bar: String = String::from("bar");
+        println!("{}", bar);
+    }
+    print_text(&foo);
+    alter_text(&mut foo);
+    println!("{}", foo);
+
+    let mut one: i8 = 1;
     {
         println!("{}", one);
-        let two: String = String::from("two");
+        let two: i8 = 2;
         println!("{}", two);
     }
-    print_number(&one);
+    print_number(one);
     alter_number(&mut one);
     println!("{}", one);
 }
