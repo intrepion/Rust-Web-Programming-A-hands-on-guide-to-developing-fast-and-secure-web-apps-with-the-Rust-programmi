259471a2875639ac48dc18a44147131436ca6f75
Oliver Forral <intrepion@gmail.com>
2021-11-15 12:10:59 -0700

    Added tests with and without thoughts

--- projects/page_021_building_structs/src/main.rs
+++ projects/page_021_building_structs/src/main.rs
@@ -1,3 +1,64 @@
+#[cfg(test)]
+mod should {
+    use super::*;
+
+    #[test]
+    fn succeed_some_function_given_name_age() {
+
+        let expected = Human {
+            name: "Oliver Forral".to_string(),
+            age: 38,
+            current_thought: "nothing".to_string(),
+        };
+
+        let actual = Human::new("Oliver Forral", 38);
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn succeed_some_function_given_name_age_current_thought() {
+
+        let expected = Human {
+            name: "Oliver Forral".to_string(),
+            age: 38,
+            current_thought: "I want to program in Rust.".to_string(),
+        };
+
+        let actual = Human::new("Oliver Forral", 38)
+            .with_thought("I want to program in Rust.".to_string());
+
+        assert_eq!(expected, actual);
+    }
+}
+
+#[derive(Debug, PartialEq)]
+struct Human {
+    name: String,
+    age: i8,
+    current_thought: String,
+}
+
+impl Human {
+
+    fn new(input_name: &str, input_age: i8) -> Human {
+
+        return Human {
+            name: input_name.to_string(),
+            age: input_age,
+            current_thought: String::from("nothing"),
+        };
+    }
+
+    fn with_thought(mut self, thought: String) -> Human {
+
+        self.current_thought = thought;
+
+        return self;
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let developer = Human::new("Maxwell Flitton", 31);
 }
