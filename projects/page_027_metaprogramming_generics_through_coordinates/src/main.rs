a9691a9dd50c73a29e3974784c61ac927fd61872
Oliver Forral <intrepion@gmail.com>
2021-11-17 22:08:19 -0700

    cargo fmt

--- projects/page_027_metaprogramming_generics_through_coordinates/src/main.rs
+++ projects/page_027_metaprogramming_generics_through_coordinates/src/main.rs
@@ -11,17 +11,17 @@ mod should {
     }
 }
 
-struct Coordinate <T> {
+struct Coordinate<T> {
     x: T,
     y: T,
 }
 
 fn some_function() {
-    let one = Coordinate{x: 50, y: 50};
+    let one = Coordinate { x: 50, y: 50 };
     println!("one: ({}, {})", one.x, one.y);
-    let two = Coordinate{x: 500, y: 500};
+    let two = Coordinate { x: 500, y: 500 };
     println!("two: ({}, {})", two.x, two.y);
-    let three = Coordinate{x: 5.6, y: 5.6};
+    let three = Coordinate { x: 5.6, y: 5.6 };
     println!("three: ({}, {})", three.x, three.y);
 }
 
