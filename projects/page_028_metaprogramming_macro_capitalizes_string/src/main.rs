b584be681e78cfc3c9e65d709e2a7178538cc777
Oliver Forral <intrepion@gmail.com>
2021-11-17 23:04:14 -0700

    macro that capitalizes

--- projects/page_028_metaprogramming_macro_capitalizes_string/src/main.rs
+++ projects/page_028_metaprogramming_macro_capitalizes_string/src/main.rs
@@ -1,3 +1,30 @@
+#[cfg(test)]
+mod should {
+    use super::*;
+
+    #[test]
+    fn succeed_some_function_given_run() {
+        let expected = ();
+        let actual = some_function();
+
+        assert_eq!(expected, actual);
+    }
+}
+
+macro_rules! capitalize {
+    ($a: expr) => {
+        let mut v: Vec<char> = $a.chars().collect();
+        v[0] = v[0].to_uppercase().nth(0).unwrap();
+        $a = v.into_iter().collect();
+    };
+}
+
+fn some_function() {
+    let mut x = String::from("test");
+    capitalize!(x);
+    println!("{}", x);
+}
+
 fn main() {
-    println!("Hello, world!");
+    some_function();
 }
