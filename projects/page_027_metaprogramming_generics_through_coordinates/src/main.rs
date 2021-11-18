08392d0540d09be7b75afd2ec84cd13553b98cbc
Oliver Forral <intrepion@gmail.com>
2021-11-17 22:03:41 -0700

    metaprogramming generics through coordinates

--- projects/page_027_metaprogramming_generics_through_coordinates/src/main.rs
+++ projects/page_027_metaprogramming_generics_through_coordinates/src/main.rs
@@ -1,3 +1,30 @@
+#[cfg(test)]
+mod should {
+    use super::*;
+
+    #[test]
+    fn succeed_some_function_given_run() {
+        let expected = ();
+        let actual = some_function();
+
+        assert_eq!(expected, actual);
+    }
+}
+
+struct Coordinate <T> {
+    x: T,
+    y: T,
+}
+
+fn some_function() {
+    let one = Coordinate{x: 50, y: 50};
+    println!("one: ({}, {})", one.x, one.y);
+    let two = Coordinate{x: 500, y: 500};
+    println!("two: ({}, {})", two.x, two.y);
+    let three = Coordinate{x: 5.6, y: 5.6};
+    println!("three: ({}, {})", three.x, three.y);
+}
+
 fn main() {
-    println!("Hello, world!");
+    some_function();
 }
