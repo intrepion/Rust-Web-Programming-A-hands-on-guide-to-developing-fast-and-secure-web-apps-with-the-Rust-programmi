0c50949d7414ad6115cbb4deb63cecab938dceb8
Oliver Forral <intrepion@gmail.com>
2021-11-18 12:39:48 -0700

    documenting with cargo

--- projects/page_037_documenting_with_cargo/src/main.rs
+++ projects/page_037_documenting_with_cargo/src/main.rs
@@ -1,3 +1,63 @@
+#[cfg(test)]
+mod should {
+    use super::*;
+
+    #[test]
+    fn succeed_generate_float_given_rng() {
+        let expected = true;
+
+        let mut rng: ThreadRng = rand::thread_rng();
+        let number = generate_float(&mut rng);
+
+        let actual = number >= 0.0 && number <= 10.0;
+
+        assert_eq!(expected, actual);
+    }
+}
+
+use rand::prelude::*;
+
+/// This function generates a float number using a number
+/// generator passed into the function.
+///
+/// # Arguments
+/// * generator (&mut ThreadRng): the random number
+/// generator to generate the random number
+///
+/// # Returns
+/// (f64): random number between 0 -> 10
+fn generate_float(generator: &mut ThreadRng) -> f64 {
+    let placeholder: f64 = generator.gen();
+
+    return placeholder * 10.0;
+}
+
+/// This trait defines the struct to be a user.
+trait IsUser {
+    /// This function proclaims that the structs is a user.
+    ///
+    /// # Arguments
+    /// None
+    ///
+    /// # Returns
+    /// (bool) true if user, false if not
+    fn is_user() -> bool {
+        return true;
+    }
+}
+/// This struct defines a user
+///
+/// # Arguments
+/// * name (String): The name of the user
+/// * age (i8): The age of the user
+struct User {
+    name: String,
+    age: i8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng: ThreadRng = rand::thread_rng();
+
+    let random_number = generate_float(&mut rng);
+    println!("{}", random_number);
 }
