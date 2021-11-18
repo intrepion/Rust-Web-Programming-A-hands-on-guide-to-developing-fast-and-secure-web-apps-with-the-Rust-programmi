4384ce70d6fd914cf059503fda4e08d3f31f62de
Oliver Forral <intrepion@gmail.com>
2021-11-18 14:47:31 -0700

    defining functionality with traits

--- projects/page_048_defining_functionality_with_traits/src/main.rs
+++ projects/page_048_defining_functionality_with_traits/src/main.rs
@@ -1,3 +1,34 @@
+#[cfg(test)]
+mod should {
+    use super::*;
+
+    #[test]
+    fn succeed_some_function_given_run() {
+        let expected = ();
+        let actual = some_function();
+
+        assert_eq!(expected, actual);
+    }
+}
+
+mod to_do;
+
+use to_do::structs::traits::create::Create;
+use to_do::to_do_factory;
+use to_do::ItemTypes;
+
+fn some_function() {
+    let to_do_item: Result<ItemTypes, &'static str> = to_do_factory("pending", "washing");
+
+    match to_do_item.unwrap() {
+        ItemTypes::Pending(item) => item.create(&item.super_struct.title),
+        ItemTypes::Done(item) => println!(
+            "it's a done item with the title: {}",
+            item.super_struct.title
+        ),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    some_function();
 }
