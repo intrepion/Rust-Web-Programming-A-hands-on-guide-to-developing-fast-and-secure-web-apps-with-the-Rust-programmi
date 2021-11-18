bc24133db4e89e7e3bdca0355c0cc959acb85e11
Oliver Forral <intrepion@gmail.com>
2021-11-18 00:12:45 -0700

    shipping crates with cargo

--- projects/page_036_shipping_crates_with_cargo/src/main.rs
+++ projects/page_036_shipping_crates_with_cargo/src/main.rs
@@ -3,16 +3,29 @@ mod should {
     use super::*;
 
     #[test]
-    fn succeed_some_function_given_run() {
-        let expected = ();
-        let actual = some_function();
+    fn succeed_generate_float_given_rng() {
+        let expected = true;
+
+        let mut rng: ThreadRng = rand::thread_rng();
+        let number = generate_float(&mut rng);
+
+        let actual = number >= 0.0 && number <= 10.0;
 
         assert_eq!(expected, actual);
     }
 }
 
-fn some_function() {}
+use rand::prelude::*;
+
+fn generate_float(generator: &mut ThreadRng) -> f64 {
+    let placeholder: f64 = generator.gen();
+
+    return placeholder * 10.0;
+}
 
 fn main() {
-    some_function();
+    let mut rng: ThreadRng = rand::thread_rng();
+
+    let random_number = generate_float(&mut rng);
+    println!("{}", random_number);
 }
