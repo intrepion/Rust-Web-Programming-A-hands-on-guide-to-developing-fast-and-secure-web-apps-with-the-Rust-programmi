54e829ff7a6ad3ab8cbfd0695f933d9cdcc19dc7
Oliver Forral <intrepion@gmail.com>
2021-11-19 07:56:30 -0700

    cargo fmt

--- projects/page_053_reading_and_writing_json_files/src/main.rs
+++ projects/page_053_reading_and_writing_json_files/src/main.rs
@@ -13,10 +13,10 @@ mod should {
 
 mod state;
 
-use std::env;
-use state::{read_file, write_to_file};
 use serde_json::value::Value;
 use serde_json::{json, Map};
+use state::{read_file, write_to_file};
+use std::env;
 
 fn some_function() {}
 
