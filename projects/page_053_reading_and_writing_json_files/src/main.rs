f86a2dcd8c3b481eb94622573ee3dd80a6952cd6
Oliver Forral <intrepion@gmail.com>
2021-11-19 07:46:14 -0700

    reading and writing json files

--- projects/page_053_reading_and_writing_json_files/src/main.rs
+++ projects/page_053_reading_and_writing_json_files/src/main.rs
@@ -1,3 +1,34 @@
+#[cfg(test)]
+mod should {
+    use super::*;
+
+    #[test]
+    fn succeed_some_function_given_run() {
+        let expected = ();
+        let actual = some_function();
+
+        assert_eq!(expected, actual);
+    }
+}
+
+mod state;
+
+use std::env;
+use state::{read_file, write_to_file};
+use serde_json::value::Value;
+use serde_json::{json, Map};
+
+fn some_function() {}
+
 fn main() {
-    println!("Hello, world!");
+    some_function();
+    let args: Vec<String> = env::args().collect();
+    let status: &String = &args[1];
+    let title: &String = &args[2];
+
+    let mut state: Map<String, Value> = read_file("./state.json");
+    println!("{:?}", state);
+
+    state.insert(title.to_string(), json!(status));
+    write_to_file("./state.json", &mut state);
 }
