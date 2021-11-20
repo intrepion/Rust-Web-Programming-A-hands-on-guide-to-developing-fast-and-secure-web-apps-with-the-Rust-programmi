f04f559e400aec202282a2feb6ee613a5588c05f
Oliver Forral <intrepion@gmail.com>
2021-11-19 22:48:34 -0700

    processing traits and structs

--- projects/page_058_processing_traits_and_structs/src/main.rs
+++ projects/page_058_processing_traits_and_structs/src/main.rs
@@ -1,3 +1,47 @@
+#[cfg(test)]
+mod should {
+    use super::*;
+
+    #[test]
+    fn succeed_some_function_given_run() {
+        let expected = ();
+        let actual = some_function();
+
+        assert_eq!(expected, actual);
+    }
+}
+
+mod state;
+mod to_do;
+mod processes;
+
+use std::env;
+use state::read_file;
+use serde_json::value::Value;
+use serde_json::Map;
+use to_do::to_do_factory;
+use processes::process_input;
+
+fn some_function() {}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let command: &String = &args[1];
+    let title: &String = &args[2];
+
+    let state: Map<String, Value> = read_file("./state.json");
+
+    let status: String;
+    match &state.get(*&title) {
+        Some(result) => {
+            status = result.to_string().replace('\"', "");
+        }
+        None => {
+            status = "pending".to_string();
+        }
+    }
+    let item = to_do_factory(&status, title).expect(&status);
+    process_input(item, command.to_string(), &state);
+    some_function();
 }
