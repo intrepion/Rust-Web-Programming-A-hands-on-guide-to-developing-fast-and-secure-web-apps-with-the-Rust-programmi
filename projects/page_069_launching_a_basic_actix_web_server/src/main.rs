f794a90876e35de1c852ef6a32cd59a84df99559
Oliver Forral <intrepion@gmail.com>
2021-11-20 00:00:05 -0700

    launching a basic Actix Web server

--- projects/page_069_launching_a_basic_actix_web_server/src/main.rs
+++ projects/page_069_launching_a_basic_actix_web_server/src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+#[cfg(test)]
+mod should {
+    use super::*;
+
+    #[test]
+    fn succeed_some_function_given_run() {
+        let expected = ();
+        let actual = some_function();
+
+        assert_eq!(expected, actual);
+    }
+}
+
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", name)
+}
+
+fn some_function() {}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    some_function();
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
 }
