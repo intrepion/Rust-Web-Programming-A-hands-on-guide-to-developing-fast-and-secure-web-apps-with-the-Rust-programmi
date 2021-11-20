9f759ba807f88c1a45ef2ed4bf493dc894b88bb6
Oliver Forral <intrepion@gmail.com>
2021-11-20 07:16:14 -0700

    cargo fmt

--- projects/page_073_understanding_asynchronous_programming/src/main.rs
+++ projects/page_073_understanding_asynchronous_programming/src/main.rs
@@ -17,7 +17,7 @@ fn do_something(number: i8) -> i8 {
     println!("number {} is running", number);
     let two_seconds = time::Duration::new(2, 0);
     thread::sleep(two_seconds);
-    return 2
+    return 2;
 }
 
 fn main() {
