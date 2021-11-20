e2ca1158fcf1c543327350e00158a327cb69cd4c
Oliver Forral <intrepion@gmail.com>
2021-11-20 07:15:58 -0700

    created synchronous function

--- projects/page_073_understanding_asynchronous_programming/src/main.rs
+++ projects/page_073_understanding_asynchronous_programming/src/main.rs
@@ -1,3 +1,31 @@
+#[cfg(test)]
+mod should {
+    use super::*;
+
+    #[test]
+    fn succeed_do_something_given_run() {
+        let expected = 2;
+        let actual = do_something(1);
+
+        assert_eq!(expected, actual);
+    }
+}
+
+use std::{thread, time};
+
+fn do_something(number: i8) -> i8 {
+    println!("number {} is running", number);
+    let two_seconds = time::Duration::new(2, 0);
+    thread::sleep(two_seconds);
+    return 2
+}
+
 fn main() {
-    println!("Hello, world!");
+    let now = time::Instant::now();
+    let one: i8 = do_something(1);
+    let two: i8 = do_something(2);
+    let three: i8 = do_something(3);
+
+    println!("time elapsed {:?}", now.elapsed());
+    println!("result {}", one + two + three);
 }
