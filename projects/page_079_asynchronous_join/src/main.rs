b2c197dd9011711293f9d0df223dda84461670f5
Oliver Forral <intrepion@gmail.com>
2021-11-20 21:01:05 -0700

    asynchronous join

--- projects/page_079_asynchronous_join/src/main.rs
+++ projects/page_079_asynchronous_join/src/main.rs
@@ -1,3 +1,48 @@
+#[cfg(test)]
+mod should {
+    use super::*;
+
+    #[test]
+    fn succeed_do_something_given_run() {
+        let expected = 2;
+        let actual = block_on(do_something(1));
+
+        assert_eq!(expected, actual);
+    }
+}
+
+use async_std;
+use futures::executor::block_on;
+use futures::future::join_all;
+use std::vec::Vec;
+use std::{thread, time};
+
+async fn do_something(number: i8) -> i8 {
+    println!("number {} is running", number);
+    let two_seconds = time::Duration::new(2, 0);
+    thread::sleep(two_seconds);
+    return 2;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let third_outcome = async {
+        let mut futures_vec = Vec::new();
+        let future_four = do_something(4);
+        let future_five = do_something(5);
+        futures_vec.push(future_four);
+        futures_vec.push(future_five);
+
+        let handles = futures_vec
+            .into_iter()
+            .map(async_std::task::spawn)
+            .collect::<Vec<_>>();
+        let results = join_all(handles).await;
+
+        return results;
+    };
+
+    let now = time::Instant::now();
+    let result = block_on(third_outcome);
+    println!("time elapsed for join vec {:?}", now.elapsed());
+    println!("here is the result: {:?}", result);
 }
