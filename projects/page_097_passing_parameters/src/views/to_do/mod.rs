7bbb0d29bbfa6d382adda67065abebb0746ae6c5
Oliver Forral <intrepion@gmail.com>
2021-11-21 00:36:37 -0700

    server that accepts a GET request and creates an entry in the JSON

---
+++ projects/page_097_passing_parameters/src/views/to_do/mod.rs
@@ -0,0 +1,9 @@
+use actix_web::web;
+mod create;
+use super::path::Path;
+
+pub fn item_factory(app: &mut web::ServiceConfig) {
+    let base_path: Path = Path{prefix: String::from("/item")};
+    app.route(&base_path.define(String::from("/create/{title}")),
+        web::get().to(create::create));
+}
