699243c68a89562e6d13fec7550421cd54100539
Oliver Forral <intrepion@gmail.com>
2021-11-20 20:09:04 -0700

    package two futures

--- projects/page_077_package_two_futures/src/main.rs
+++ projects/page_077_package_two_futures/src/main.rs
@@ -1,3 +1,33 @@
+#[cfg(test)]
+mod should {
+    use super::*;
+
+    #[test]
+    fn succeed_do_something_given_run() {
+        let expected = 2;
+        let actual = block_on(do_something(1));
+
+        assert_eq!(expected, actual);
+    }
+}
+
+use futures::executor::block_on;
+use std::{thread, time};
+
+async fn do_something(number: i8) -> i8 {
+    println!("number {} is running", number);
+    let two_seconds = time::Duration::new(2, 0);
+    thread::sleep(two_seconds);
+    return 2;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let future_two = async {
+        let outcome_two = do_something(2).await;
+        let outcome_three = do_something(3).await;
+        return [outcome_two, outcome_three];
+    };
+
+    let future_two = block_on(future_two);
+    println!("here is the outcome: {:?}", future_two);
 }
