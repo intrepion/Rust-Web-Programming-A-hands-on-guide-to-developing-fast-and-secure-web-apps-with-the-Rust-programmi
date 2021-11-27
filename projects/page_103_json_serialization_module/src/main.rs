cd497f8c3403522d8928d03398d8b5c90e2d1957
Oliver Forral <intrepion@gmail.com>
2021-11-26 22:36:09 -0800

    json serialization module

--- projects/page_103_json_serialization_module/src/main.rs
+++ projects/page_103_json_serialization_module/src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+#[cfg(test)]
+mod should {
+    use super::*;
+
+    #[test]
+    fn succeed_some_function_given_run() {
+        let expected = ();
+        let actual = some_function();
+
+        assert_eq!(expected, actual);
+    }
+}
+
+use actix_web::{App, HttpServer};
+mod json_serialization;
+mod processes;
+mod state;
+mod to_do;
+mod views;
+
+fn some_function() {}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    some_function();
+    HttpServer::new(|| {
+        let app = App::new().configure(views::views_factory);
+
+        return app;
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
