20cdf09029ffec9cea604daf4100900098259012
Oliver Forral <intrepion@gmail.com>
2021-11-29 01:26:19 -0800

    injecting javascript into html

--- projects/page_127_injecting_javascript_into_html/src/views/to_do/create.rs
+++ projects/page_127_injecting_javascript_into_html/src/views/to_do/create.rs
@@ -1,18 +1,18 @@
 use crate::processes::process_input;
 use crate::state::read_file;
 use crate::to_do;
-use actix_web::HttpRequest;
+use super::utils::return_state;
+use actix_web::{HttpRequest, HttpResponse};
 use serde_json::value::Value;
 use serde_json::Map;
 
-pub async fn create(req: HttpRequest) -> String {
+pub async fn create(req: HttpRequest) -> HttpResponse {
     let state: Map<String, Value> = read_file("./state.json"); // 1
 
     let title: String = req.match_info().get("title").unwrap().to_string();
-    let title_reference: String = title.clone(); // 2
 
     let item = to_do::to_do_factory(&String::from("pending"), &title).expect("create "); // 3
     process_input(item, "create".to_string(), &state); // 4
 
-    return format!("{} created", title_reference); // 5
+    return HttpResponse::Ok().json(return_state());
 }
