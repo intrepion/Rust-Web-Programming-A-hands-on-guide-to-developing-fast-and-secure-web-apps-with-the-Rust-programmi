44659844410c09608af0d522c341671d54609fcd
Oliver Forral <intrepion@gmail.com>
2021-11-28 16:43:41 -0800

    basic index page

--- projects/page_120_displaying_html_in_a_browser/src/views/mod.rs
+++ projects/page_120_displaying_html_in_a_browser/src/views/mod.rs
@@ -1,10 +1,12 @@
 use actix_web::web;
+mod app;
 mod auth;
 mod path;
 mod to_do;
 pub mod token;
 
 pub fn views_factory(app: &mut web::ServiceConfig) {
+    app::app_factory(app);
     auth::auth_factory(app);
     to_do::item_factory(app);
 }
