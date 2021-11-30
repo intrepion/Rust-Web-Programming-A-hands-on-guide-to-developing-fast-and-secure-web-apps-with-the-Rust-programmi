835a5b5641e4bb97f1b8df88cc2f0e3421b0a0ae
Oliver Forral <intrepion@gmail.com>
2021-11-29 19:45:21 -0800

    inserting data into the database

--- projects/page_167_inserting_data_into_the_database/src/views/to_do/create.rs
+++ projects/page_167_inserting_data_into_the_database/src/views/to_do/create.rs
@@ -1,18 +1,29 @@
 use super::utils::return_state;
-use crate::processes::process_input;
-use crate::state::read_file;
-use crate::to_do;
-use actix_web::{HttpRequest, HttpResponse};
-use serde_json::value::Value;
-use serde_json::Map;
+use crate::database::establish_connection;
+use crate::diesel;
+use crate::models::item::item::Item;
+use crate::models::item::new_item::NewItem;
+use crate::schema::to_do;
+use actix_web::{HttpRequest, Responder};
+use diesel::prelude::*;
 
-pub async fn create(req: HttpRequest) -> HttpResponse {
-    let state: Map<String, Value> = read_file("./state.json"); // 1
-
+pub async fn create(req: HttpRequest) -> impl Responder {
     let title: String = req.match_info().get("title").unwrap().to_string();
+    let title_ref: String = title.clone();
 
-    let item = to_do::to_do_factory(&String::from("pending"), &title).expect("create "); // 3
-    process_input(item, "create".to_string(), &state); // 4
+    let connection = establish_connection();
+    let items = to_do::table
+        .filter(to_do::columns::title.eq(title_ref.as_str()))
+        .order(to_do::columns::id.asc())
+        .load::<Item>(&connection)
+        .unwrap();
 
-    return HttpResponse::Ok().json(return_state());
+    if items.len() == 0 {
+        let new_post = NewItem::new(title);
+        let _ = diesel::insert_into(to_do::table)
+            .values(&new_post)
+            .execute(&connection);
+    }
+
+    return return_state();
 }
