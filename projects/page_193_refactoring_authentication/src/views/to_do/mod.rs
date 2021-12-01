4521fe37ac267432a71d4e4edc9e7f3b6d5555b3
Oliver Forral <intrepion@gmail.com>
2021-12-01 15:07:03 -0700

    copying code from previous project

---
+++ projects/page_193_refactoring_authentication/src/views/to_do/mod.rs
@@ -0,0 +1,35 @@
+mod create;
+mod delete;
+mod edit;
+mod get;
+mod utils;
+
+use super::path::Path;
+use actix_web::web;
+
+pub fn item_factory(app: &mut web::ServiceConfig) {
+    let base_path: Path = Path {
+        prefix: String::from("/item"),
+    };
+
+    app.route(
+        &base_path.define(String::from("/create/{title}")),
+        web::post().to(create::create),
+    );
+
+    app.route(
+        &base_path.define(String::from("/get")),
+        web::get().to(get::get),
+    );
+
+    // add inside the item_factory function
+    app.route(
+        &base_path.define(String::from("/edit")),
+        web::put().to(edit::edit),
+    );
+
+    app.route(
+        &base_path.define(String::from("/delete")),
+        web::post().to(delete::delete),
+    );
+}
