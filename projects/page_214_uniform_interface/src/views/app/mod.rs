8783fb4d95f8c0c9524da05242b62cde076348d2
Oliver Forral <intrepion@gmail.com>
2021-12-03 00:57:38 -0700

    copying code from previous project

---
+++ projects/page_214_uniform_interface/src/views/app/mod.rs
@@ -0,0 +1,27 @@
+mod content_loader;
+mod items;
+mod login;
+mod logout;
+use super::path::Path;
+use actix_web::web;
+
+pub fn app_factory(app: &mut web::ServiceConfig) {
+    let base_path: Path = Path {
+        prefix: String::from("/"),
+    };
+
+    app.route(
+        &base_path.define(String::from("")),
+        web::get().to(items::items),
+    );
+
+    app.route(
+        &base_path.define(String::from("login/")),
+        web::get().to(login::login),
+    );
+
+    app.route(
+        &base_path.define(String::from("logout/")),
+        web::get().to(logout::logout),
+    );
+}
