29cc67974d6dd01f67b0180ca95746a6ec388bc6
Oliver Forral <intrepion@gmail.com>
2021-12-03 20:57:42 -0700

    copied code from previous project

---
+++ projects/page_231_caching/javascript/main.js
@@ -0,0 +1,98 @@
+if (localStorage.getItem("user-token") == null) {
+    window.location.replace(document.location.origin + "/login/");
+}
+
+function renderItems(items, processType, elementId, processFunction) {
+    let placeholder = "<div>";
+    let itemsMeta = [];
+    for (i = 0; i < items.length; i += 1) {
+        let title = items[i]["title"];
+        let placeholderId = processType + "-" + title.replaceAll(" ", "-");
+
+        placeholder += '<div class="itemContainer">'
+            + '<p>'
+            + title
+            + '</p>'
+            + '<div class="actionButton" '
+            + 'id="'
+            + placeholderId
+            + '">'
+            + processType
+            + '</div>'
+            + "</div>";
+
+        itemsMeta.push({ "id": placeholderId, "title": title });
+    }
+    placeholder += "</div>";
+    document.getElementById(elementId).innerHTML = placeholder;
+
+    for (i = 0; i < itemsMeta.length; i += 1) {
+        document.getElementById(itemsMeta[i]["id"]).addEventListener("click", processFunction);
+    }
+}
+
+function apiCall(url, method) {
+    let xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+    xhr.addEventListener('readystatechange', function () {
+        if (this.readyState === this.DONE) {
+            if (this.status === 401) {
+                window.location.replace(document.location.origin + "/login/");
+            } else {
+                renderItems(JSON.parse(this.responseText)["pending_items"],
+                    "edit", "pendingItems", editItem);
+                renderItems(JSON.parse(this.responseText)["done_items"],
+                    "delete", "doneItems", deleteItem);
+                document.getElementById("completeNum").innerHTML =
+                    JSON.parse(this.responseText)["done_item_count"];
+                document.getElementById("pendingNum").innerHTML =
+                    JSON.parse(this.responseText)["pending_item_count"];
+            }
+        }
+    });
+    xhr.open(method, "/api/v1" + url);
+    xhr.setRequestHeader('content-type', 'application/json');
+    xhr.setRequestHeader('user-token', localStorage.getItem("user-token"));
+
+    return xhr;
+}
+
+function editItem() {
+    let title = this.id.replaceAll("-", " ").replace("edit ", "");
+    let call = apiCall("/item/edit", "PUT");
+    let json = {
+        "title": title,
+        "status": "done",
+    };
+    call.send(JSON.stringify(json));
+}
+
+function deleteItem() {
+    let title = this.id.replaceAll("-", " ").replace("delete ", "");
+    let call = apiCall("/item/delete", "POST");
+    let json = {
+        "title": title,
+        "status": "done",
+    };
+    call.send(JSON.stringify(json));
+}
+
+function getItems() {
+    let call = apiCall("/item/get", 'GET');
+    call.send();
+}
+
+if (localStorage.getItem("user-token") == null) {
+    window.location.replace(document.location.origin + "/login/");
+} else {
+    getItems();
+}
+
+document.getElementById("create-button").addEventListener("click", createItem);
+
+function createItem() {
+    let title = document.getElementById("name");
+    let call = apiCall("/item/create/" + title.value, "POST");
+    call.send();
+    document.getElementById("name").value = null;
+}
--- projects/page_231_caching/src/main.rs
+++ projects/page_231_caching/src/main.rs
@@ -1,3 +1,78 @@
-fn main() {
-    println!("Hello, world!");
+#[cfg(test)]
+mod should {
+    use super::*;
+
+    #[test]
+    fn succeed_some_function_given_run() {
+        let expected = ();
+        let actual = some_function();
+
+        assert_eq!(expected, actual);
+    }
+}
+
+#[macro_use]
+extern crate diesel;
+extern crate dotenv;
+
+mod auth;
+mod database;
+mod json_serialization;
+mod models;
+mod processes;
+mod schema;
+mod state;
+mod to_do;
+mod views;
+use actix_service::Service;
+use actix_web::{App, HttpResponse, HttpServer};
+use env_logger;
+use futures::future::{ok, Either};
+use log;
+
+fn some_function() {}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    some_function();
+    env_logger::init();
+    HttpServer::new(|| {
+        let app = App::new()
+            .wrap_fn(|req, srv| {
+                let request_url: String = String::from(*&req.uri().path().clone());
+                let passed: bool;
+
+                if *&req.path().contains("/item/") {
+                    match auth::process_token(&req) {
+                        Ok(_token) => {
+                            passed = true;
+                        }
+                        Err(_message) => {
+                            passed = false;
+                        }
+                    }
+                } else {
+                    passed = true;
+                }
+
+                let end_result = match passed {
+                    true => Either::Left(srv.call(req)),
+                    false => Either::Right(ok(
+                        req.into_response(HttpResponse::Unauthorized().finish().into_body())
+                    )),
+                };
+
+                async move {
+                    let result = end_result.await?;
+                    log::info!("{} -> {}", request_url, &result.status());
+                    Ok(result)
+                }
+            })
+            .configure(views::views_factory);
+
+        return app;
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
