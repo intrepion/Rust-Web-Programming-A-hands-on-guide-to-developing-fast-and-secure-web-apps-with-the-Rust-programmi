379464b9f011c19e013fcc968156ad0d95b67c55
Oliver Forral <intrepion@gmail.com>
2021-12-03 22:37:01 -0700

    copying code from previous project

---
+++ projects/page_242_building_our_unit_tests/src/auth/jwt.rs
@@ -0,0 +1,53 @@
+extern crate hmac;
+extern crate jwt;
+extern crate sha2;
+use actix_web::HttpRequest;
+use hmac::{Hmac, NewMac};
+use jwt::SignWithKey;
+use jwt::{Header, Token, VerifyWithKey};
+use sha2::Sha256;
+use std::collections::BTreeMap;
+
+pub struct JwtToken {
+    pub user_id: i32,
+    pub body: String,
+}
+
+impl JwtToken {
+    pub fn encode(user_id: i32) -> String {
+        let key: Hmac<Sha256> = Hmac::new_varkey(b"secret").unwrap();
+        let mut claims = BTreeMap::new();
+        claims.insert("user_id", user_id);
+        let token_str: String = claims.sign_with_key(&key).unwrap();
+
+        return token_str;
+    }
+
+    pub fn decode(encoded_token: String) -> Result<JwtToken, &'static str> {
+        let key: Hmac<Sha256> = Hmac::new_varkey(b"secret").unwrap();
+        let token_str: &str = encoded_token.as_str();
+
+        let token: Result<Token<Header, BTreeMap<String, i32>, _>, _> =
+            VerifyWithKey::verify_with_key(token_str, &key);
+
+        match token {
+            Ok(token) => {
+                let _header = token.header();
+                let claims = token.claims();
+
+                return Ok(JwtToken {
+                    user_id: claims["user_id"],
+                    body: encoded_token,
+                });
+            }
+            Err(_) => return Err("Could not decode"),
+        }
+    }
+
+    pub fn decode_from_request(request: HttpRequest) -> Result<JwtToken, &'static str> {
+        match request.headers().get("user-token") {
+            Some(token) => JwtToken::decode(String::from(token.to_str().unwrap())),
+            None => Err("there is no token"),
+        }
+    }
+}
---
+++ projects/page_242_building_our_unit_tests/src/views/users/mod.rs
@@ -0,0 +1,15 @@
+mod create;
+use super::path::Path;
+use actix_web::web;
+
+pub fn user_factory(app: &mut web::ServiceConfig) {
+    let base_path: Path = Path {
+        prefix: String::from("/user"),
+        backend: true,
+    };
+
+    app.route(
+        &base_path.define(String::from("/create")),
+        web::post().to(create::create),
+    );
+}
