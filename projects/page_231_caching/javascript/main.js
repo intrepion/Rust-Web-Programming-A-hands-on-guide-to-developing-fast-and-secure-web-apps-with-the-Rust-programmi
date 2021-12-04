2e35b70143501eb0d623b6f5dc529751f4d31971
Oliver Forral <intrepion@gmail.com>
2021-12-03 21:56:59 -0700

    caching

--- projects/page_231_caching/javascript/main.js
+++ projects/page_231_caching/javascript/main.js
@@ -39,14 +39,9 @@ function apiCall(url, method) {
             if (this.status === 401) {
                 window.location.replace(document.location.origin + "/login/");
             } else {
-                renderItems(JSON.parse(this.responseText)["pending_items"],
-                    "edit", "pendingItems", editItem);
-                renderItems(JSON.parse(this.responseText)["done_items"],
-                    "delete", "doneItems", deleteItem);
-                document.getElementById("completeNum").innerHTML =
-                    JSON.parse(this.responseText)["done_item_count"];
-                document.getElementById("pendingNum").innerHTML =
-                    JSON.parse(this.responseText)["pending_item_count"];
+                runRenderProcess(JSON.parse(this.responseText));
+                localStorage.setItem("item-cache-date", new Date());
+                localStorage.setItem("item-cache-data", this.responseText);
             }
         }
     });
@@ -85,7 +80,15 @@ function getItems() {
 if (localStorage.getItem("user-token") == null) {
     window.location.replace(document.location.origin + "/login/");
 } else {
-    getItems();
+    let cachedDate = Date.parse(localStorage.getItem("item-cache-date"));
+    let now = new Date();
+    let difference = Math.round((now - cachedDate) / 1000);
+
+    if (difference <= 120) {
+        runRenderProcess(JSON.parse(localStorage.getItem("item-cache-data")));
+    } else {
+        getItems();
+    }
 }
 
 document.getElementById("create-button").addEventListener("click", createItem);
@@ -95,4 +98,13 @@ function createItem() {
     let call = apiCall("/item/create/" + title.value, "POST");
     call.send();
     document.getElementById("name").value = null;
+}
+
+function runRenderProcess(data) {
+    renderItems(data["pending_items"], "edit", "pendingItems", editItem);
+    renderItems(data["done_items"], "delete", "doneItems", deleteItem);
+    document.getElementById("completeNum").innerHTML =
+        data["done_item_count"];
+    document.getElementById("pendingNum").innerHTML =
+        data["pending_item_count"];
 }
