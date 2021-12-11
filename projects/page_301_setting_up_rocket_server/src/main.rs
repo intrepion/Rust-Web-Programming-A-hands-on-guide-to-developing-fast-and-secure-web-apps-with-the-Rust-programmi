96f42e30414f9dc18c606e744dce8a35d645d47e
Oliver Forral <intrepion@gmail.com>
2021-12-11 03:18:41 -0700

    setting up rocket server

--- projects/page_301_setting_up_rocket_server/src/main.rs
+++ projects/page_301_setting_up_rocket_server/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+#[get("/hello/<name>/<age>")]
+fn hello(name: String, age: u8) -> String {
+    format!("Hello, {} year old named {}!", age, name)
+}
+
+#[get("/bye/<name>/<age>")]
+fn bye(name: String, age: u8) -> String {
+    format!("Goodbye, {} year old named {}!", age, name)
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![hello, bye])
 }
